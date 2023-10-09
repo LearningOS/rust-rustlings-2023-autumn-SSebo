79c4bfdd72716dd313cf55b393466ac577b69391
ssebo <shibo501c@gmail.com>
2023-10-09 19:40:06 +0800

    95

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +51,30 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let tokens: Vec<&str> = s.trim().split(",").collect();
+
+        if tokens.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = tokens[0].to_string();
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        match tokens[1].parse::<usize>() {
+            Ok(age) => {
+                return Ok(Person {
+                    name,
+                    age,
+                });
+            }
+            Err(e) => Err(ParsePersonError::ParseInt(e))
+        }
     }
 }
 
@@ -68,6 +91,7 @@ mod tests {
     fn empty_input() {
         assert_eq!("".parse::<Person>(), Err(ParsePersonError::Empty));
     }
+
     #[test]
     fn good_input() {
         let p = "John,32".parse::<Person>();
@@ -76,6 +100,7 @@ mod tests {
         assert_eq!(p.name, "John");
         assert_eq!(p.age, 32);
     }
+
     #[test]
     fn missing_age() {
         assert!(matches!(
